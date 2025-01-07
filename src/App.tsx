6ce7cd34f247e39913970f3f948aa904d18fce1c
Samriddha Malla <samriddhamalla99@gmail.com>
2025-01-07 14:48:14 -0500

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ function App() {
   return (
     <>
       <h1>project initialization</h1>
+      <div className="flex items-center justify-center h-screen bg-gray-200">
+        <h1 className="text-4xl font-bold text-blue-500">
+          Hello, Tailwind CSS!
+        </h1>
+      </div>
     </>
   );
 }
